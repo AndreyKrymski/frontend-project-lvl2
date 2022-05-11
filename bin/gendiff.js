02867fed1e71ef9a8261f469e56348ff56340442
Andrey Krymski <6714223@mail.ru>
2022-05-11 11:21:09 +0300

    шаг 1

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { Command, Option } from 'commander';
+import genDIff from '../src/index.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .version('0.0.2')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .addOption(new Option('-f, --format <type>', 'output format').choices(['stylish', 'plain', 'json']).default('stylish'))
+  .action((filepath1, filepath2, options) => {
+    console.log(genDIff(filepath1, filepath2, options.format));
+  });
+
+program.parse();
