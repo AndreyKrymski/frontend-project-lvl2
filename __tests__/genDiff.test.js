be9f01908b65c3c567ffd763a78baa6b44e75d1d
Andrey Krymski <6714223@mail.ru>
2022-05-22 13:04:30 +0300

    tests gendiff

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,4 +1,7 @@
 import func from '../parsers/exampleFunction.js';
+import jsonParsers from '../parsers/jsonParser.js';
+import logicJson from '../parsers/logicJson.js';
+import genDiff from '../src/index.js';
 
 const file1 = {
   host: 'hexlet.io',
@@ -6,6 +9,8 @@ const file1 = {
   proxy: '123.234.53.22',
   follow: false,
 };
+const result2 = ['   host: hexlet.io', ' - verbose: true', ' + timeout: 20', ' - timeout: 50', ' - follow: false',
+  ' - proxy: 123.234.53.22'];
 const file2 = {
   timeout: 20,
   verbose: true,
@@ -19,7 +24,21 @@ const result = `{
  - timeout: 50
  - verbose: true
 }`;
+describe('my tests', () => {
+  test('func', () => {
+    expect(func(file1, file2)).toEqual(result);
+  });
 
-test('gennDiff', () => {
-  expect(func(file1, file2)).toEqual(result);
+  test('jsonParsers', () => {
+    expect(jsonParsers('__fixtures__/file1.json')).toEqual(file1);
+    expect(jsonParsers('__fixtures__/file2.json')).toEqual(file2);
+  });
+
+  test('logicJson', () => {
+    expect(logicJson(result2)).toEqual(result);
+  });
+
+  test('genDiff', () => {
+    expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
+  });
 });
