d67668d85bcf1e0b0505438a9600623473e124e9
Andrey Krymski <6714223@mail.ru>
2022-05-23 21:31:36 +0300

    ghjk

--- parsers/exampleFunction.js
+++ parsers/exampleFunction.js
@@ -1,29 +1,28 @@
 import _ from 'lodash';
-import func2 from './logicJson.js';
 
 const func = (object1, object2) => {
-  const keys = _.union(_.keys(object1), _.keys(object2));
+  const keys = _.union(_.keys(object1), _.keys(object2)).sort();
 
   const massiv = keys.flatMap((item) => {
     const value1 = object1[item];
     const value2 = object2[item];
-    const itemValue = (key, value, sign = ' ') => ` ${sign} ${key}: ${value}`;
-    const positivSign = '+';
-    const negativeSign = '-';
 
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { item, children: func(value1, value2) };
+    }
     if (_.has(object1, item) && !_.has(object2, item)) {
-      return itemValue(item, value1, negativeSign);
+      return { item, values: value1, sign: '-' };
     }
     if (!_.has(object1, item) && _.has(object2, item)) {
-      return itemValue(item, value2, positivSign);
+      return { item, values: value2, sign: '+' };
     }
     if (_.has(object1, item) && _.has(object2, item)) {
       if (value1 === value2) {
-        return itemValue(item, value1);
+        return { item, values: value1, sign: ' ' };
       }
-      return [itemValue(item, value1, negativeSign), itemValue(item, value2, positivSign)];
+      return [{ item, values: value2, sign: '+' }, { item, values: value1, sign: '-' }];
     } return 'Eror, file contains internal files';
   });
-  return func2(massiv);
+  return massiv;
 };
 export default func;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import func from '../parsers/exampleFunction.js';
 const genDiff = (filepath1, filepath2) => {
   const file1 = jsonParser(filepath1);
   const file2 = jsonParser(filepath2);
-  return func(file1, file2);
+  const ans = func(file1, file2);
+  return ans;
 };
 export default genDiff;
